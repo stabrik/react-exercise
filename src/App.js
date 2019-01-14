e82375cbc6774201a625ac30b361f713dbcc2e57
stabrik <katarzynastabryla@gmail.com>
2019-01-14 20:52:48 +0100

    menu - desktop,mobile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Navig } from './components/nav/nav.js';
-
+import './style/style.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
-
         <header className="App-header">
           <Navig />
         </header>
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React from 'react';
+//import $ from 'jquery';
 
 export const Navig = () => {
+
     return (
+        // <div className="nav">
+        //             <a className="nav__link nav__link--active" href="#" onClick={changeLink}>Inwestycja</a>
+        //             <a className="nav__link" href="#">Dostępność</a>
+        //             <a className="nav__link" href="#">Rzuty mieszkań</a>
+        //             <a className="nav__link" href="#">Kontakt</a>
+        // </div>
         <div>
-            <ul className="nav justify-content-center">
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">Inwestycja</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Dostępność</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Rzuty mieszkań</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Kontakt</a>
-                </li>
-            </ul>
+            <nav className="navbar navbar-expand-md">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-angle-down"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Inwestycja <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Dostępność</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" tabindex="-1" aria-disabled="true">Rzuty mieszkań</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#" tabindex="-1" aria-disabled="true">Kontakt</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
         </div>
     );
 };
